66613cf5f48e06f229c76aae19b9961503a4948b
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-03-28 18:35:15 +1100

    Revert "try settings gradle"
    This reverts commit 113439c0c745838d67e2ce3c2e1db6043f34a4a1.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,18 +3,3 @@ plugins {
 }
 rootProject.name = "gradle-publisher"
 
-val jfrogUser = gradle.startParameter.projectProperties["jfrogUser"]
-val jfrogPassword = gradle.startParameter.projectProperties["jfrogPassword"]
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot")
-
-            credentials {
-                username = jfrogUser
-                password = jfrogPassword
-            }
-        }
-        gradlePluginPortal()
-    }
-}
