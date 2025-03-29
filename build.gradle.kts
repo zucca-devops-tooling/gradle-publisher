ad9bc2bcc586d3badb3094f0add034209954fbc2
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-03-29 15:52:38 +1100

    fix plugin after working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     `kotlin-dsl`
-    id("com.zucca.gradle-publisher") version "1.0.0-SNAPSHOT"
+    id("java-gradle-plugin")
+    id("maven-publish")
 }
 
 group = "com.zucca"
@@ -28,8 +29,24 @@ kotlin {
     jvmToolchain(17)
 }
 
-publisher {
-    devRepoUrl = "https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot"
-    usernameProperty = "jfrogUser"
-    passwordProperty = "jfrogPassword"
+gradlePlugin {
+    plugins {
+        create("gradlePublisherPlugin") {
+            id = "com.zucca.gradle-publisher"
+            implementationClass = "com.zucca.GradlePublisherPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "ArtifactorySnapshots"
+            url = uri("https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot")
+            credentials {
+                username = project.findProperty("jfrogUser") as String?
+                password = project.findProperty("jfrogPassword") as String?
+            }
+        }
+    }
 }
