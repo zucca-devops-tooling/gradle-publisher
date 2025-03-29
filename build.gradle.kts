70a1e6572880873aa5ded4c15ffe9ae1c8f38181
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-03-29 15:15:29 +1100

    retry with only one snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     `kotlin-dsl`
-    id("java-gradle-plugin")
-    id("maven-publish")
+    id("com.zucca.gradle-publisher") version "1.0.0-SNAPSHOT"
 }
 
 group = "com.zucca"
@@ -29,24 +28,8 @@ kotlin {
     jvmToolchain(17)
 }
 
-gradlePlugin {
-    plugins {
-        create("gradlePublisherPlugin") {
-            id = "com.zucca.gradle-publisher"
-            implementationClass = "com.zucca.GradlePublisherPlugin"
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "ArtifactorySnapshots"
-            url = uri("https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot")
-            credentials {
-                username = project.findProperty("jfrogUser") as String?
-                password = project.findProperty("jfrogPassword") as String?
-            }
-        }
-    }
+publisher {
+    devRepoUrl = "https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot"
+    usernameProperty = "jfrogUser"
+    passwordProperty = "jfrogPassword"
 }
