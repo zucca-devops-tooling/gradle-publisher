63957b3cf2d5d3ffacc18abe3196825e6b59d482
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-03-29 14:49:33 +1100

    attempt to set publisher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     `kotlin-dsl`
-    id("java-gradle-plugin")
-    id("maven-publish")
+    id("com.zucca.gradle-publisher") version "1.0.0-SNAPSHOT"
 }
 
 group = "com.zucca"
@@ -29,44 +28,8 @@ kotlin {
     jvmToolchain(17)
 }
 
-gradlePlugin {
-    plugins {
-        create("gradlePublisherPlugin") {
-            id = "com.zucca.gradle-publisher"
-            implementationClass = "com.zucca.GradlePublisherPlugin"
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "ArtifactorySnapshots"
-            url = uri("https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot")
-            credentials {
-                username = project.findProperty("jfrogUser") as String?
-                password = project.findProperty("jfrogPassword") as String?
-            }
-        }
-    }
-
-    publications {
-        create<MavenPublication>("manualPluginMarker") {
-            groupId = "com.zucca"
-            artifactId = "gradle-publisher.gradle.plugin"
-            version = "1.0.0-SNAPSHOT"
-            pom {
-                packaging = "pom"
-                withXml {
-                    asNode().appendNode("dependencies").apply {
-                        appendNode("dependency").apply {
-                            appendNode("groupId", "com.zucca")
-                            appendNode("artifactId", "gradle-publisher")
-                            appendNode("version", "1.0.0-SNAPSHOT")
-                        }
-                    }
-                }
-            }
-        }
-    }
+publisher {
+    devRepoUrl = "https://zuccadevops.jfrog.io/artifactory/publisher-libs-snapshot"
+    usernameProperty = "jfrogUser"
+    passwordProperty = "jfrogPassword"
 }
