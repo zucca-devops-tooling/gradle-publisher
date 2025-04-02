e9ce76f670ad2511a5db989d096b99c612b8e804
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-04-03 09:46:56 +1100

    fix Unresolved reference by transforming class instance to Unit

--- src/main/kotlin/configuration/PluginConfiguration.kt
+++ src/main/kotlin/configuration/PluginConfiguration.kt
@@ -7,6 +7,14 @@ open class PluginConfiguration @Inject constructor(objects: ObjectFactory) {
     val dev = objects.newInstance(RepositoryConfig::class.java)
     val prod = objects.newInstance(RepositoryConfig::class.java)
 
+    fun dev(configure: RepositoryConfig.() -> Unit) {
+        dev.configure()
+    }
+
+    fun prod(configure: RepositoryConfig.() -> Unit) {
+        prod.configure()
+    }
+
     // Shared fallback credentials
     var usernameProperty: String? = Defaults.USER_PROPERTY
     var passwordProperty: String? = Defaults.PASS_PROPERTY
