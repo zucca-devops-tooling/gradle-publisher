9594567b157858dcbac8142e8231abc896c2905d
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-04-05 21:19:47 +1100

    try to push to central

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,25 +15,27 @@ pipeline {
         stage('Build & Test') {
             environment {
                 JFROG_CREDENTIALS = credentials('jfrog-user-pass')
+                OSSRH_CREDENTIALS = credentials('OSSRH_CREDENTIALS')
+                GPG_SECRET_KEY = credentials('GPG_SECRET_KEY')
+                GPG_KEY_ID = credentials('GPG_KEY_ID')
+                GPG_KEY_PASS = credentials('GPG_KEY_PASS')
             }
             steps {
                 sh """
                     ./gradlew clean build test --refresh-dependencies \
                         -PjfrogUser=$JFROG_CREDENTIALS_USR \
-                        -PjfrogPassword=$JFROG_CREDENTIALS_PSW
+                        -PjfrogPassword=$JFROG_CREDENTIALS_PSW \
+                        -PossrhUser=$OSSRH_CREDENTIALS_USR \
+                        -PossrhPassword=$OSSRH_CREDENTIALS_PSW
+
                 """
             }
         }
 
         stage('Publish to Artifactory') {
-            environment {
-                JFROG_CREDENTIALS = credentials('jfrog-user-pass')
-            }
             steps {
                 sh """
-                    ./gradlew publish --info \
-                        -PjfrogUser=$JFROG_CREDENTIALS_USR \
-                        -PjfrogPassword=$JFROG_CREDENTIALS_PSW
+                    ./gradlew publish --info
                 """
             }
         }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     `kotlin-dsl`
-    id("dev.zucca-ops.gradle-publisher") version "0.0.1-maven-central-workaround-SNAPSHOT"
+    id("dev.zucca-ops.gradle-publisher") version "0.0.1-PR-6-SNAPSHOT"
     id("java-gradle-plugin")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    signing
 }
 
 group = "dev.zucca-ops"
@@ -49,6 +51,65 @@ publisher {
         customGradleCommand = "publishToSonatype"
     }
 
+    releaseBranchPatterns = listOf("PR-6")
     usernameProperty = "ossrhUser"
     passwordProperty = "ossrhPassword"
+}
+
+afterEvaluate {
+    extensions.configure<PublishingExtension> {
+        publications {
+            named<MavenPublication>("maven") {
+                pom {
+                    name.set("Gradle Publisher")
+                    description.set("A Gradle plugin that simplifies publishing by detecting environment and routing to the correct repository with dynamic versions.")
+                    url.set("https://github.com/zucca-devops-tooling/gradle-publisher")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("zucca")
+                            name.set("Guido Zuccarelli")
+                            email.set("guidozuccarelli@hotmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/zucca-devops-tooling/gradle-publisher.git")
+                        developerConnection.set("scm:git:ssh://github.com/zucca-devops-tooling/gradle-publisher.git")
+                        url.set("https://github.com/zucca-devops-tooling/gradle-publisher")
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Signing
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("SIGNING_KEY_ID"),
+        System.getenv("SIGNING_KEY"),
+        System.getenv("SIGNING_PASSWORD")
+    )
+
+    val publishing = extensions.getByType<PublishingExtension>()
+    sign(publishing.publications["maven"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            username.set(findProperty("ossrhUser") as String)
+            password.set(findProperty("ossrhPassword") as String)
+        }
+    }
 }
