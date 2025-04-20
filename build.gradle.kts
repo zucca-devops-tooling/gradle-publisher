b748a03368312a72075e475e378b91354e3603cb
Guido Zuccarelli <guidozuccarelli@hotmail.com>
2025-04-20 14:29:42 +1000

    test usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.20"
     `kotlin-dsl`
-    id("dev.zucca-ops.gradle-publisher") version "0.1.1"
+    id("dev.zucca-ops.gradle-publisher") version "0.1.1-PR-27-SNAPSHOT"
     id("java-gradle-plugin")
     signing
     id("com.diffplug.spotless") version "7.0.3"
